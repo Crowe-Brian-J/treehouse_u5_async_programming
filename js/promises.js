ff1e5563dd88078525b7d39970ca71bb904f7357
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-25 22:19:38 -0400

    use promises.js to return array

--- js/promises.js
+++ js/promises.js
@@ -1,23 +1,38 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json'
+const astrosUrl = 'astros.json'
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
 const peopleList = document.getElementById('people')
 const btn = document.querySelector('button')
 
-const getJSON = (url, callback) => {
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', url)
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText)
-      return callback(data)
+// Make an AJAX request
+const getJSON = (url) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText)
+        resolve(data)
+      } else {
+        reject(Error(xhr.statusText))
+      }
     }
-  }
-  xhr.send()
-}
-const getProfile = (json) => {
-  json.people.map((person) => {
-    getJSON(wikiUrl + person.name, generateHTML)
+    xhr.onerror = () => {
+      reject(Error('A network error occurred.'))
+    }
+    xhr.send()
   })
+}
+
+//map over all astronauts
+const getProfiles = (json) => {
+  const profiles = json.people.map((person) => {
+    if (person.wiki) {
+      return getJSON(wikiUrl + person.wiki, generateHTML)
+    } else {
+      return getJSON(wikiUrl + person.name, generateHTML)
+    }
+  })
+  return profiles
 }
 
 // Generate the markup for each profile
@@ -27,7 +42,9 @@ const generateHTML = (data) => {
   // Check if request returns a 'standard' page from Wiki
   if (data.type === 'standard') {
     section.innerHTML = `
-      <img src=${data.thumbnail.source}>
+      <img src="${data.thumbnail.source || 'img/profile.jpg'}" alt="${
+      data.title
+    }">
       <h2>${data.title}</h2>
       <p>${data.description}</p>
       <p>${data.extract}</p>
@@ -37,12 +54,15 @@ const generateHTML = (data) => {
       <img src="img/profile.jpg" alt="ocean clouds seen from space">
       <h2>${data.title}</h2>
       <p>Results unavailable for ${data.title}</p>
-      ${data.extract_html}
+      ${data.extract_html || ''}
     `
   }
 }
 
-btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles)
-  event.target.remove()
+btn.addEventListener('click', (e) => {
+  getJSON(astrosUrl)
+    .then(getProfiles)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+  e.target.remove()
 })
