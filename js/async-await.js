dc27ea7e0a01fad0be9810611649ed943c4d4577
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-25 23:33:23 -0400

    async-await exercise

--- js/async-await.js
+++ js/async-await.js
@@ -1,35 +1,63 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json'
+const astrosUrl = 'astros.json'
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
 const peopleList = document.getElementById('people')
 const btn = document.querySelector('button')
 
 // Handle all fetch requests
+const getPeopleInSpace = async (url) => {
+  const peopleResponse = await fetch(url)
+  const peopleJSON = await peopleResponse.json()
+
+  //map over people
+  const profiles = peopleJSON.people.map(async (person) => {
+    const perName = person.name
+    const craft = person.craft
+    const wikiUrlFinal = person.wiki ? person.wiki : person.name
+
+    const profileResponse = await fetch(
+      wikiUrl + encodeURIComponent(wikiUrlFinal)
+    )
+    const profileJSON = await profileResponse.json()
+
+    return { ...profileJSON, craft, perName }
+  })
+
+  return Promise.all(profiles)
+}
 
 // Generate the markup for each profile
 const generateHTML = (data) => {
-  data.map((person) => {
+  data.forEach((person) => {
     const section = document.createElement('section')
     peopleList.appendChild(section)
-    // Check if request returns a 'standard' page from Wiki
+
     if (person.type === 'standard') {
       section.innerHTML = `
-        <img src=${person.thumbnail.source}>
-        <span>${person.craft}</span>
-        <h2>${person.title}</h2>
-        <p>${person.description}</p>
-        <p>${person.extract}</p>
+        <img src="${person.thumbnail?.source || 'img/profile.jpg'}" alt="${
+        person.title
+      }">
+        <span>${person.craft || ''}</span>
+        <h2>${person.perName || person.title}</h2>
+        <p>${person.description || ''}</p>
+        <p>${person.extract || ''}</p>
       `
     } else {
       section.innerHTML = `
         <img src="img/profile.jpg" alt="ocean clouds seen from space">
-        <h2>${person.title}</h2>
+        <span>${person.craft || ''}</span>
+        <h2>${person.perName || person.title}</h2>
         <p>Results unavailable for ${person.title}</p>
-        ${person.extract_html}
+        ${person.extract_html || ''}
       `
     }
   })
 }
 
-btn.addEventListener('click', (event) => {
-  event.target.textContent = 'Loading...'
+btn.addEventListener('click', async (e) => {
+  e.target.textContent = 'Loading...'
+
+  const astros = await getPeopleInSpace(astrosUrl)
+  generateHTML(astros)
+
+  e.target.remove()
 })
